15b272e9bf600787f35612b1103c99fd0ef5f372
Ramakrishna0010 <127001869+Ramakrishna0010@users.noreply.github.com>
2023-07-14 22:54:37 +0530

    Update swtich_case.cpp

--- swtich_case.cpp
+++ swtich_case.cpp
@@ -1,1 +1,14 @@
+// int a,b,c,d;
 
+// cout<<"give four integers as input: a,b,c,d"<<endl;
+// cin>>a>>b>>c>>d;
+
+// if(a>b && c>d){
+//     cout<<"Haha";
+// }
+//     else if(a>b && c<d){
+//         cout<<"Hehe";
+//     }
+//     else if(a<b && c>d){
+//         cout<<"Huhu";
+//     }
